038d1be7da963e716e749ba7f936e3eee730ddd6
Dang Khoa <khoavodang1451997@gmail.com>
2022-05-16 17:40:26 +0700

    final

---
+++ src/main.js
@@ -0,0 +1,34 @@
+// Sticky Nav
+let navbar = $(".navbar");
+$(window).scroll(function(){
+    let oTop = $(".section-2").offset().top - window.innerHeight;
+    if ($(window).scrollTop()> oTop) {
+        navbar.addClass("sticky");
+    } else {
+        navbar.removeClass("sticky")
+    }
+
+})
+
+// Counter Animation
+let nCount = function(selector) {
+    $(selector).each(function() {
+        S(this).animate({
+            Counter: $(this).text()
+        },{
+            duration:4000,
+            easing:"swing",
+            step:function(value) {
+                $(this).text(Math.cell(value))
+            }
+        })
+    })
+}
+let a = 0;
+$(window).scroll(function(){
+    let oTop = $(".numbers").offset().top - window.innerHeight;
+    if(a == 0 && $(window).scrollTop()>= oTop) {
+        a++;
+        nCount(".rect > h1");
+    }
+})
